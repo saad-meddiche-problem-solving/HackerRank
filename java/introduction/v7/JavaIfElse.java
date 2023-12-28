89e17486a33ebe25533f4c8707a201cdb60bd00c
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-28 13:46:18 +0100

    Add JavaIfElse problem

--- java/introduction/v7/JavaIfElse.java
+++ java/introduction/v7/JavaIfElse.java
@@ -1,16 +1,31 @@
 package java.introduction.v7;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class JavaIfElse {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        if(N % 2 != 0){
+            System.out.println("Weird");
+        }else {
+            if(2 <= N && N <= 5){
+                System.out.println("Not Weird");
+            }
 
-        bufferedReader.close();
+            if(6 <= N && N <= 20){
+                System.out.println("Weird");
+            }
+
+            if(N > 20){
+                System.out.println("Not Weird");
+            }
+
+        }
+
+        scanner.close();
     }
 }
