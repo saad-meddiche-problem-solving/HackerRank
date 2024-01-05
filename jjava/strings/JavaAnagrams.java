0331a3f7176a809b04013ddfbcb548e805723cf1
saadMeddiche <saadmeddiche2004201@gmail.com>
2024-01-05 12:19:31 +0100

    java anagrams

---
+++ jjava/strings/JavaAnagrams.java
@@ -0,0 +1,56 @@
+package jjava.strings;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class JavaAnagrams {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+        String b = scanner.next();
+        scanner.close();
+
+        System.out.println(
+                isAnagram(a, b) ? "Anagrams" : "Not Anagrams"
+        );
+    }
+
+    public static Boolean isAnagram(String a, String b) {
+
+        char[] charArrayA = a.toLowerCase().toCharArray();
+        char[] charArrayB = b.toLowerCase().toCharArray();
+
+        Arrays.sort(charArrayA);
+        Arrays.sort(charArrayB);
+
+        return Arrays.equals(charArrayA, charArrayB);
+
+    }
+
+}
+
+class DeclarativeApproachJavaAnagrams{
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+        String b = scanner.next();
+        scanner.close();
+
+        System.out.println(
+                isAnagram(a, b) ? "Anagrams" : "Not Anagrams"
+        );
+    }
+
+    public static Boolean isAnagram(String a, String b) {
+
+      return Arrays.equals(
+              a.toLowerCase().chars().sorted().toArray(),
+              b.toLowerCase().chars().sorted().toArray()
+      );
+    }
+
+}
