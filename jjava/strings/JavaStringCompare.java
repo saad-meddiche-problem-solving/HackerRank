854ac70d90f7f75c0c9e561926f873d9f30e5644
saadMeddiche <saadmeddiche2004201@gmail.com>
2024-01-05 11:31:58 +0100

    Java String Compare (In Declarative Approach)

--- jjava/strings/JavaStringCompare.java
+++ jjava/strings/JavaStringCompare.java
@@ -1,6 +1,9 @@
 package jjava.strings;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 
 public class JavaStringCompare {
 
@@ -38,6 +41,7 @@ public class JavaStringCompare {
     public static void getLargestAndSmallest(){
 
         Object[] arrayList = listofSubtrings.toArray();
+
         Arrays.sort(arrayList);
 
         smallSubString = (String) arrayList[0];
@@ -54,5 +58,59 @@ public class JavaStringCompare {
     public static void scanData() {
         S = scanner.next();K = scanner.nextInt();
     }
+
+}
+
+class DeclarativeApproach{
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String inputString = scanner.next();
+        int substringLength = scanner.nextInt();
+
+        List<String> substrings = generateSubstrings(inputString, substringLength);
+
+        Pair<String, String> smallestAndLargest = getLargestAndSmallest(substrings);
+
+        System.out.println("Smallest Substring: " + smallestAndLargest.getFirst());
+        System.out.println("Largest Substring: " + smallestAndLargest.getSecond());
+
+    }
+
+    public static List<String> generateSubstrings(String text , Integer substringLenght){
+        return IntStream.range(0 , text.length() - substringLenght + 1)
+                .mapToObj(i -> text.substring(i , i + substringLenght))
+                .collect(Collectors.toList());
+    }
+
+    public static Pair<String , String> getLargestAndSmallest(List<String> list){
+        Collections.sort(list);
+        return new Pair<>(list.get(0) ,list.get(list.size() - 1) );
+    }
+
+    static class Pair<A , B>{
+
+        private A first;
+
+        private B second;
+
+        public Pair(A first , B second){
+            this.first = first;
+            this.second = second;
+        }
+
+        public A getFirst(){
+            return first;
+        }
+
+        public B getSecond(){
+            return second;
+        }
+
+    }
+
+
 
 }
