377519602cf3665d9438e732caf20ad546407139
saadMeddiche <saadmeddiche2004201@gmail.com>
2025-05-12 16:08:54 +0100

    Add MyCalculator class and refactor JavaExceptionHandling to demonstrate exception handling with power method

--- jjava/exception_handling/JavaExceptionHandling.java
+++ jjava/exception_handling/JavaExceptionHandling.java
@@ -2,32 +2,36 @@ package jjava.exception_handling;
 
 import java.util.Scanner;
 
-// https://www.hackerrank.com/challenges/java-exception-handling-try-catch/problem?isFullScreen=true
+// https://www.hackerrank.com/challenges/java-exception-handling/problem?isFullScreen=true
+class MyCalculator {
+
+    long power(int n, int p) throws Exception {
+
+        if(n < 0 || p < 0)
+            throw new Exception("n or p should not be negative.");
+
+        if(n == 0 && p == 0 )
+            throw new Exception("n and p should not be zero.");
+
+        return (long) Math.pow( (double) n, (double) p);
+
+    }
+}
+
 public class JavaExceptionHandling {
+    public static final MyCalculator my_calculator = new MyCalculator();
+    public static final Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        while (in .hasNextInt()) {
+            int n = in .nextInt();
+            int p = in .nextInt();
 
-        int x , y;
-        try{
-
-            x = scanner.nextInt();
-            y = scanner.nextInt();
-
-        } catch(Exception ex) {
-            System.out.println(ex.getClass().toString().replace("class ",""));
-            return;
+            try {
+                System.out.println(my_calculator.power(n, p));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
         }
-
-        try {
-
-            int result = x / y;
-            System.out.println(result);
-
-        } catch(Exception ex){
-            System.out.println(ex);
-            return;
-        }
-
     }
 }
---
+++ jjava/exception_handling/JavaExceptionHandlingTryCatch.java
@@ -0,0 +1,33 @@
+package jjava.exception_handling;
+
+import java.util.Scanner;
+
+// https://www.hackerrank.com/challenges/java-exception-handling-try-catch/problem?isFullScreen=true
+public class JavaExceptionHandlingTryCatch {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x , y;
+        try{
+
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+
+        } catch(Exception ex) {
+            System.out.println(ex.getClass().toString().replace("class ",""));
+            return;
+        }
+
+        try {
+
+            int result = x / y;
+            System.out.println(result);
+
+        } catch(Exception ex){
+            System.out.println(ex);
+            return;
+        }
+
+    }
+}
