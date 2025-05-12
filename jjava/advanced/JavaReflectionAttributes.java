e01b39003b817a27995f2f58dcca06809803bb78
saadMeddiche <saadmeddiche2004201@gmail.com>
2025-05-12 16:25:55 +0100

    Add JavaReflectionAttributes class to demonstrate reflection and method listing in Java

--- jjava/advanced/JavaReflectionAttributes.java
+++ jjava/advanced/JavaReflectionAttributes.java
@@ -1,5 +1,36 @@
 package jjava.advanced;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+
 // https://www.hackerrank.com/challenges/java-reflection-attributes/problem
 public class JavaReflectionAttributes {
+
+    public static void main(String[] args){
+        Class student = Student.class;
+        Method[] methods = student.getDeclaredMethods();
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for(Method method : methods){
+            methodList.add(method.getName());
+        }
+        Collections.sort(methodList);
+        for(String name: methodList){
+            System.out.println(name);
+        }
+    }
+
+    class Student {
+        public void getName() {
+        }
+        public void setId(String id) {
+
+        }
+        public void setEmail(String email) {
+
+        }
+        public void anothermethod() {  }
+    }
+
 }
