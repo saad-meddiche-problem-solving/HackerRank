3271f97f32c37622baaec39bc73c8ac033c15a0d
saadMeddiche <saadmeddiche2004201@gmail.com>
2025-05-13 09:09:56 +0100

    Fix prime number check to handle all non-positive integers

--- jjava/advanced/v15/PrimeChecker.java
+++ jjava/advanced/v15/PrimeChecker.java
@@ -23,11 +23,11 @@ class Prime {
 
     private boolean isPrime(int num) {
 
-        if(num == 0 || num == 1)
+        if(num <= 1)
             return false;
 
         for(int i = 2 ; i <= Math.sqrt(num); i++)
-            if(num % 2 == 0) return false;
+            if(num % i == 0) return false;
 
         return true;
     }
